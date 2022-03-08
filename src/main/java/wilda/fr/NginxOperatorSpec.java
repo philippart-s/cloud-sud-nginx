3ca5b6206d28e85dd85b92d6dc35af44ca3d02ae
philippart-s <wildagsx@gmail.com>
2022-03-08 13:57:02 +0100

    📄 CRD generation

---
+++ src/main/java/wilda/fr/NginxOperatorSpec.java
@@ -0,0 +1,14 @@
+package wilda.fr;
+
+public class NginxOperatorSpec {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
